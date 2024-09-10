f18aa2db6f1aead78be8eea33fc0cad21363557c
Rincon Nunes <Rinconnunes@live.co.uk>
2024-09-10 19:42:09 +0100

    update revert

--- lambda/lamda_function.py
+++ lambda/lamda_function.py
@@ -1,7 +1,7 @@
 import json
 
-def lambda_handler(event, context):
-    # TODO implement
+def lambda_handler (event, context):
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from our CICD github actions workflow vscode updated version')
